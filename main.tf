930df3a50ca7e3df11c79ef5678f0ace9813c7cd
Rafael Pierre <rafael.pierre@databricks.com>
2023-05-08 14:53:29 +0200

    clean

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ resource "azurerm_resource_group" "example" {
   location = "Australia East"
 }
 
+resource "azurerm_resource_group" "example2" {
+  name     = "example-rg-workshop2"
+  location = "Australia East"
+}
+
 provider "azurerm" {
    features {}
 }
@@ -11,7 +16,7 @@ resource "azurerm_databricks_workspace" "example" {
   name                = "databricks-infra-workshop"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku                 = "standard"
+  sku                 = "premium"
 
   tags = {
     Environment = "Production"
